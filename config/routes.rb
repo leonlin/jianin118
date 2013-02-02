eeb3fc03355c42f55305f566e3347460c64f3855
kyk <linjiansuper@gmail.com>
2013-02-02 17:42:22 +0000

    style update

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ KykBlog::Application.routes.draw do
 
   root to: 'static_pages#home'
   match '/about', to: 'static_pages#about'
-  # match '/signup',  to: 'users#new'
+  #match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
 
